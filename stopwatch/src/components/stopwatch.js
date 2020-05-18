a60418c9279e504ad0a561eb9f57b7db33b3dea4
may55 <mayank.soni055@gmail.com>
2020-05-18 22:26:27 +0530

    Final stopwatch

--- stopwatch/src/components/stopwatch.js
+++ stopwatch/src/components/stopwatch.js
@@ -12,6 +12,44 @@ class Stopwatch extends Component {
     }
   }
 
+  formatter = (input) => {
+    const time = input.toString();
+    return time.length < 2 ? '0' + time : time.slice(-2);
+  }
+
+  formatTime = () => {
+    this.setState({
+      milliSeconds: this.formatter(this.state.timePassed),
+      seconds: this.formatter(Math.floor((this.state.timePassed / 100) % 60)),
+      minutes: this.formatter(Math.floor((this.state.timePassed / (100 * 60)) % 60))
+    });
+  }
+
+  timeNow = () => {
+    let seconds = parseInt(this.state.timePassed, 10) + 1;
+    this.setState({ timePassed: seconds });
+    this.formatTime();
+  }
+
+  startTime = () => {
+    if(this.state.isStarted===true) {return; }
+    this.setState({ timePassed: 0,
+                    milliSeconds: '00',
+                    seconds: '00',
+                    minutes: '00',
+                    isStarted: true, 
+    });
+    this.interval = setInterval(this.timeNow, 10);
+  }
+
+  
+
+  stopTime = () => {
+    window.clearInterval(this.interval);
+    this.setState({ isStarted: false,           
+    });
+  }
+
   render(){
     let {milliSeconds} = this.state;
     let {seconds} = this.state;
@@ -24,8 +62,12 @@ class Stopwatch extends Component {
           {minutes} - {seconds} - {milliSeconds}
         </div>
         <div className="stopwatch-btn">
-          <div className="btn btn-primary">Start</div>
-          <div className="btn btn-danger">Stop</div>
+          <div className="btn btn-primary" 
+          onClick={this.startTime.bind(this)}>Start
+          </div>
+          <div className="btn btn-danger" 
+          onClick={this.stopTime.bind(this)}>Stop
+          </div>
         </div>
       </div>
     )
